e9d7527c99ea2dea3050cf5a3e7400cca397845d
RamiMata <ramirosanra@hotmail.com>
2024-11-18 16:49:48 -0300

    Add files via upload

---
+++ Desafio 1 (clase 2).py
@@ -0,0 +1,16 @@
+lista_1 = [1,2,3]
+lista_2 = ["a","b","c"]
+
+lista_1.append(456789)
+lista_1.append("Hola Mundo")
+
+lista_2.append("Hola y adios")
+lista_2.append("5555")
+
+lista_3 = lista_1[:-1]
+
+lista_4 = lista_2[1:-1]
+
+lista_5 = lista_3 + lista_4
+
+
